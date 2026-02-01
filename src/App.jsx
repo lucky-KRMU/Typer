cf346f9ae923fd82a56b73919d76fd6f5cd1ab52
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-01 14:08:48 +0000

    fix stale state

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import Hero from './Components/Hero/Hero';
 function App() {
   let para1 = {
         1: "Morning light spills across quiet streets,",
-        2: " Birds announce daylight with cheerful persistence,",
+        2: "Birds announce daylight with cheerful persistence,",
         3: "Coffee brews patiently filling kitchens with warmth,",
         4: "Windows open slowly welcoming fresh hopeful air,",
         5: "People stretch yawn and gather scattered thoughts,",
         6: "Plans form gently without urgency or noise,",
         7: "Time feels generous before obligations start knocking,",
-        8: " Small rituals anchor minds for demanding hours,",
-        9: " Hope sneaks in alongside ordinary simple moments,",
-        10: " The day begins quietly promising movement and change"
+        8: "Small rituals anchor minds for demanding hours,",
+        9: "Hope sneaks in alongside ordinary simple moments,",
+        10: "The day begins quietly promising movement and change"
     }
   let para2 = {
     1: "Screens glow softly in darkened rooms,",
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,61 +1,60 @@
-import {React, useState} from 'react'
+import { React, useState, useEffect } from 'react'
 
-function TypingArea (typingtext) {
+function TypingArea(typingtext) {
     return (
         <input id="typing-area" className="h-[10vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center text-white font-semibold py-3 cursor-pointer" value={typingtext.text} onChange={typingtext.onType} />
     );
 }
 
 function Hero(HeroObj) {
-  let lines = HeroObj.para;
+    let lines = HeroObj.para;
 
-  let [lineOne, setLineOne] = useState(lines['1']);
-  let [lineTwo, setLineTwo] = useState(lines['2']);
+    let [lineOne, setLineOne] = useState(lines[1]);
+    let [lineTwo, setLineTwo] = useState(lines[2]);
 
-  let [typeArea, setTypeArea] = useState('');
+    let [linesCount, setLinesCount] = useState(3);
 
-  let firstKey = true;
-  let timeOut = false;
-  let lineCount = 3;
+    let [typeArea, setTypeArea] = useState('');
 
+    let firstKey = true;
+    let timeOut = false;
 
-
-  const handleType = (e) => {
-    // if (firstKey && !timeOut){
+    
+    const handleType = (e) => {
+        // if (firstKey && !timeOut){
         firstKey = !firstKey;
         let typeStr = e.target.value;
         setTypeArea(typeStr);
-    //}
-  }
+        //}
+    }
 
-  const handleKey = (e) => {
-    // if (!timeOut){
-        if (!timeOut && e.key === "Enter"){
-            setTypeArea("");
-            setLineOne(lines[lineCount - 1]);
-            setLineTwo(lines[lineCount]);
-            lineCount+=1
-            // console.log(lineTwo)
+    const handleKey = (e) => {
+        // if (!timeOut){
+        if (!timeOut && e.key == "Enter" && linesCount < 11) {
+            setTypeArea('');
+            setLineOne(lines[linesCount - 1]);
+            setLineTwo(lines[linesCount]);
+            setLinesCount(val => val + 1)
         }
-    // }
-  }
-//   window.addEventListener('keypress', handleType)
+        // }
+    }
+    //   window.addEventListener('keypress', handleType)
 
-  return (
-    <>
-    <div className='h-[85vh]   bg-linear-65 from-teal-200 to-blue-400 flex flex-col justify-center items-center'>
-        <div id="text-card" className='h-[60vh] w-[80vw] bg-blue-900 rounded-4xl flex flex-col gap-4 justify-center items-center'>
-            <div id="typing-text" className="h-[40vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center flex flex-col items-center justify-center gap-3 font-semibold ">
-                <div id="line-1">{lineOne}</div>
-                <div id="line-1">{lineTwo}</div>
+    return (
+        <>
+            <div className='h-[85vh]   bg-linear-65 from-teal-200 to-blue-400 flex flex-col justify-center items-center'>
+                <div id="text-card" className='h-[60vh] w-[80vw] bg-blue-900 rounded-4xl flex flex-col gap-4 justify-center items-center'>
+                    <div id="typing-text" className="h-[40vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center flex flex-col items-center justify-center gap-3 font-semibold ">
+                        <div id="line-1">{lineOne}</div>
+                        <div id="line-1">{lineTwo}</div>
+                    </div>
+                    {/* <TypingArea text={typeArea} onType={handleType} /> */}
+                    <input id="typing-area" className="h-[10vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center text-white font-semibold py-3 cursor-pointer focus:outline-none" placeholder="Type Here to Start..." value={typeArea} onChange={handleType} onKeyDown={handleKey} />
+
+                </div>
             </div>
-            {/* <TypingArea text={typeArea} onType={handleType} /> */}
-            <input id="typing-area" className="h-[10vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center text-white font-semibold py-3 cursor-pointer focus:outline-none" value={typeArea} onChange={handleType} onKeyDown={handleKey}/>
-            
-        </div>
-    </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Hero
