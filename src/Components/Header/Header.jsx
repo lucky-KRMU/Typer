60e0e9c44779c482e5becf78776ec134a77cd4dc
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-01 06:24:15 +0000

    add header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header () {
+  return (
+    <>
+    <h1 className="text-center text-teal-200 bg-blue-950 font-extrabold font-sans text-6xl py-5">Typer</h1>
+    </>
+  )
+}
+
+export default Header;
