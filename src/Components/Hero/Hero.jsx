3c494e3cde04742b8452215e20bbfbcb70a4715d
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-06 13:52:19 +0000

    inclucate useAccuracy

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import { React, useState, useEffect } from 'react';
-import Report from "../Report/Report"
+import Report from "../Report/Report";
+import useAccuracy from '../../Hooks/useAccuracy';
 
 
 // Component for typing area (input field) for taking the input from the user.
@@ -19,12 +20,13 @@ function Hero(HeroObj) {
     let [sentencesArray, setSentencesArray] = useState([]); // state variable to store the input from the use to check it's accuracy later on
 
     let [wpmText, setWpmText] = useState(15); // the initial typing time set 
-    let [timerPrefix, setTimerPrefix] = useState('Time left: ');
 
     let [typeArea, setTypeArea] = useState(''); // the state variable store the typing area text
 
     let [firstKey, setFirstKey] = useState(true); // checking for the first key 
     let [timeOut, setTimeOut] = useState(false); // state variable to check the time out
+
+    let [report, setReport] = useState([1,2,3]);
 
 
     
@@ -43,6 +45,7 @@ function Hero(HeroObj) {
             const timer = setTimeout(()=>{  // start the setTimeout function
                 setTimeOut(true); // setting the timeout to be true
                 setWpmText(0);  
+                setReport(useAccuracy(lines, sentencesArray, 15));
             }, 15000);
             
             const timeUpdater = setInterval(()=>{   // starting the interval function to update the time left
@@ -92,8 +95,8 @@ function Hero(HeroObj) {
     return (
         <>
             <div className='h-[85vh]   bg-linear-65 from-teal-200 to-blue-400 flex flex-col justify-center items-center'>
-                <Report></Report>
-                <p className="text-center p-3 text-white font-bold text-4xl">{timerPrefix} {wpmText}s</p>
+                <Report accuracy={report[0]} errors={report[2]} wpm={report[1]} />
+                <p className="text-center p-3 text-white font-bold text-4xl">Your Time: {wpmText}s</p>
                 <div id="text-card" className='h-[60vh] w-[80vw] bg-blue-900 rounded-4xl flex flex-col gap-4 justify-center items-center'>
                     <div id="typing-text" className="h-[40vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center flex flex-col items-center justify-center gap-3 font-semibold ">
                         <div id="line-1">{lineOne}</div>
--- src/Components/Report/Report.jsx
+++ src/Components/Report/Report.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Report(report) {
   return (
     <>
-    <h3 className='text-center bg-teal-900 w-full text-white font-bold text-2xl p-2'>Your Accuracy: {report.accuracy} || WPM: {report.wpm} || Errors: {report.errors}</h3>
+    <h3 className='text-center bg-teal-900 w-full text-white font-bold text-2xl p-2'>Your Accuracy: {report.accuracy}% || WPM: {report.wpm} || Errors: {report.errors}</h3>
     </>
   )
 };
