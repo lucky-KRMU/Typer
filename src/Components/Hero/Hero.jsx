748b08d9661059984bb3a5390580310be91a0606
lucky-KRMU <2501730142@krmu.edu.in>
2026-02-06 09:30:38 +0000

    add comments

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,25 +1,31 @@
 import { React, useState, useEffect } from 'react'
 
+
+// Component for typing area (input field) for taking the input from the user.
 function TypingArea(typingtext) {
     return (
-        <input id="typing-area" className="h-[10vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center text-white font-semibold py-3 cursor-pointer" value={typingtext.text} onChange={typingtext.onType} />
+        <input id="typing-area" className="h-[10vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center text-white font-semibold py-3 cursor-pointer focus:outline-none" placeholder="Type Here to Start..." value={typingtext.inputVal} onChange={typingtext.onHandleType} onKeyDown={typingtext.onHandleKey} />
     );
 }
 
 function Hero(HeroObj) {
-    let lines = HeroObj.para;
+    let lines = HeroObj.para; // taking the paragraph from HeroObj as a prop
 
-    let [lineOne, setLineOne] = useState(lines[1]);
-    let [lineTwo, setLineTwo] = useState(lines[2]);
+    let [lineOne, setLineOne] = useState(lines[1]); // Active first displaying line
+    let [lineTwo, setLineTwo] = useState(lines[2]); // Active second displaying line
 
-    let [linesCount, setLinesCount] = useState(3);
-    let [sentencesArray, setSentencesArray] = useState([]);
+    let [linesCount, setLinesCount] = useState(3);  // State variable to update the actively displaying line
+    let [sentencesArray, setSentencesArray] = useState([]); // state variable to store the input from the use to check it's accuracy later on
 
-    let [typeArea, setTypeArea] = useState('');
+    let [wpmText, setWpmText] = useState(15); // the initial typing time set 
 
-    let firstKey = true;
-    let [timeOut, setTimeOut] = useState(false);
+    let [typeArea, setTypeArea] = useState(''); // the state variable store the typing area text
 
+    let firstKey = true; // checking for the first key 
+    let [timeOut, setTimeOut] = useState(false); // state variable to check the time out
+    let [timerCountUpdater , setTimerCountUpdater] = useState(0); // timer function (state variable)
+
+  
     const startTimer = () => { 
     setTimeout(()=>{
         setTimeOut(true);
@@ -27,27 +33,32 @@ function Hero(HeroObj) {
     }
     ,15000)}
 
-    
+    useEffect(() => {
+
+    }, [timerCountUpdater])
+
+    // function to handle the typing in typing area
     const handleType = (e) => {
         if (firstKey){
             firstKey = !firstKey;
-            startTimer();
+            startTimer(); // start the timer function if first key is preseed
         }
         if (!timeOut){
             let typeStr = e.target.value;
-
-            setTypeArea(typeStr);
+            setTypeArea(typeStr); // if time is still not out, allow user to type.
         }
+
     }
 
+    // function to implement whenever the value is changed/updated in the typing area 
     const handleKey = (e) => {
         let tempSentence = '';
         let tempSentencesArray = sentencesArray;
         if (!timeOut && e.key == "Enter" && linesCount < 11) {
             tempSentence += typeArea;
-            let prev = [tempSentence]
-            tempSentencesArray.push(prev)
-            setSentencesArray(tempSentencesArray)
+            let prev = [tempSentence];
+            tempSentencesArray.push(prev);
+            setSentencesArray(tempSentencesArray);
             setTypeArea('');
             setLineOne(lines[linesCount - 1]);
             setLineTwo(lines[linesCount]);
@@ -59,14 +70,13 @@ function Hero(HeroObj) {
     return (
         <>
             <div className='h-[85vh]   bg-linear-65 from-teal-200 to-blue-400 flex flex-col justify-center items-center'>
+                <p className="text-center p-3 text-white font-bold text-4xl">Time left: {wpmText}s</p>
                 <div id="text-card" className='h-[60vh] w-[80vw] bg-blue-900 rounded-4xl flex flex-col gap-4 justify-center items-center'>
                     <div id="typing-text" className="h-[40vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center flex flex-col items-center justify-center gap-3 font-semibold ">
                         <div id="line-1">{lineOne}</div>
                         <div id="line-1">{lineTwo}</div>
                     </div>
-                    {/* <TypingArea text={typeArea} onType={handleType} /> */}
-                    <input id="typing-area" className="h-[10vh] w-[60vw] bg-teal-400 rounded-4xl text-4xl text-center text-white font-semibold py-3 cursor-pointer focus:outline-none" placeholder="Type Here to Start..." value={typeArea} onChange={handleType} onKeyDown={handleKey} />
-
+                    <TypingArea inputVal={typeArea} onHandleType={handleType} onHandleKey={handleKey} />
                 </div>
             </div>
         </>
